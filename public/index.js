c4040db9364476f188705510042f111245fed96b
Yunju <dbswndigh@kaist.ac.kr>
2019-05-11 14:55:06 +0900

    mainpage paging added

--- public/index.js
+++ public/index.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-
-	console.log("hello");
-    
+   
     let config = {
         apiKey: "AIzaSyAJYY0Az45sH0Sw6jAz3JvR4yjJBIMtbzU",
         authDomain: "squad-67b43.firebaseapp.com",
@@ -10,28 +8,36 @@ $(document).ready(function() {
     firebase.initializeApp(config);
     let database = firebase.database();
 
-    // Show all entries from Firebase
-	show_all_entries = function(){
-		return database.ref("/Offers/").once("value", function(snapshot) {
-			var my_value = snapshot.val();
-			if(my_value == null){
+    show_nth_page = function(n){ /* n is active page number */
+    	return database.ref("/Offers/").once("value", function(snapshot) {
+
+	    	var value = snapshot.val();
+			if(value == null){
 				return;
 			}
+			var key_list = Object.keys(value);
 
-			var key_list = Object.keys(my_value);
-			for(var i=0; i<key_list.length; i++){
-				var entry = my_value[key_list[i]];
+	    	var bottom = key_list.length-5-(4*n);
+	    	if(bottom < -1){
+	    		bottom = -1;
+	    	}
+
+
+	    	let entries_div = document.createElement('div');
+	    	entries_div.className = "entries";
+	    	$("#table_header").after(entries_div);
+
+
+	    	for(var i=(key_list.length-1-(4*n)); i>bottom; i--){
+				var entry = value[key_list[i]];
 
 				let new_container = document.createElement('div');
 				new_container.className = "ui container segment";
-				
-				let new_form = document.createElement('div');
-				new_form.className = "ui form";
-				new_container.appendChild(new_form);
+				entries_div.appendChild(new_container);
 
 				let new_three_col_grid = document.createElement('div');
 				new_three_col_grid.className = "ui three column stackable center aligned grid";
-				new_form.appendChild(new_three_col_grid);
+				new_container.appendChild(new_three_col_grid);
 
 				let new_middle_row = document.createElement('div');
 				new_middle_row.className = "middle aligned row";
@@ -46,30 +52,52 @@ $(document).ready(function() {
 				period.className = "column";
 				let start_date = entry.start.split('-');
 				let end_date = entry.end.split('-');
-        		period.innerHTML = "<h2>" + start_date[0] + ". " + start_date[1] + ". " + start_date[2] + " ~ " + end_date[0] + ". " + end_date[1] + ". " + end_date[2] + "</h2>";
-        		new_middle_row.appendChild(period);
+	    		period.innerHTML = "<h2>" + start_date[0] + ". " + start_date[1] + ". " + start_date[2] + " ~ " + end_date[0] + ". " + end_date[1] + ". " + end_date[2] + "</h2>";
+	    		new_middle_row.appendChild(period);
 
 				let day_and_time = document.createElement('div');
 				day_and_time.className = "column";
 
 				snapshot.child(key_list[i]).child('time').forEach(function(data){
 					let new_time = document.createElement('h2');
-		        	console.log(data.val().day+"."+data.val().start+"~"+data.val().end);
+		        	//console.log(data.val().day+"."+data.val().start+"~"+data.val().end);
 		        	new_time.innerHTML = data.val().day+"."+data.val().start+"~"+data.val().end;
 		        	new_time.className = "elem";
 		        	day_and_time.appendChild(new_time);
 				});
 		        
 		        new_middle_row.appendChild(day_and_time);
+				//console.log(entry);	
+			}
 
-        		$(".twelve.wide.column").append(new_container);
-				//console.log(entry);
+			/* Implementing Paging */
+			let center_div = document.createElement('div');
+			center_div.className = "center-div";
+			entries_div.appendChild(center_div);
+
+			let pagination = document.createElement('div');
+			pagination.className = "ui pagination menu";
+			center_div.appendChild(pagination);
 				
+
+			for(var i=0; i<(key_list.length/4); i++){
+				//console.log(i+"th page button is made");
+				if(i==n)
+					$(".ui.pagination.menu").append("<a id='page"+i+"' class='active item'>"+(i+1)+"</a>");
+				else
+					$(".ui.pagination.menu").append("<a id='page"+i+"' class='item'>"+(i+1)+"</a>");
+				$("#page"+i).on("click",function(){
+					if(this.getAttribute("class")!="active item"){
+						//console.log("clicked not-active item");
+						/* removing all entries */
+						$(".entries").remove();
+						//console.log(this.getAttribute("id")[4]);
+						show_nth_page(this.getAttribute("id")[4]);
+					}	
+				});	
 			}
 		});
-	}
+    }
 
-	show_all_entries();
-
-    
+	show_nth_page(0);
 });
